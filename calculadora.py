91451452aea76aac07f53766927cf1e99be2bf24
LarissaGuaman1 <lds.guaman@yavirac.edu.ec>
2026-06-02 23:29:31 -0500

    Agrega función de resta a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,15 @@
 def suma(a, b):
     return a + b
 
+def resta(a, b):
+    return a - b
+
 def main():
     print("=== Calculadora Colaborativa ===")
     x = float(input("Primer número: "))
     y = float(input("Segundo número: "))
     print("Suma:", suma(x, y))
+    print("Resta:", resta(x, y))
 
 if __name__ == "__main__":
     main()
