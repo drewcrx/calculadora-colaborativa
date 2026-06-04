67d5de3e8f4f399265c51d2319fa738e8ba33a00
Andrew <abv.carrera@yavirac.edu.ec>
2026-06-03 19:31:31 -0500

    Agrega función de multiplicación a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -4,12 +4,16 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def multiplicacion(a, b):
+    return a * b
+
 def main():
     print("=== Calculadora Colaborativa ===")
     x = float(input("Primer número: "))
     y = float(input("Segundo número: "))
     print("Suma:", suma(x, y))
     print("Resta:", resta(x, y))
+    print("Multiplicación:", multiplicacion(x, y))
 
 if __name__ == "__main__":
     main()
